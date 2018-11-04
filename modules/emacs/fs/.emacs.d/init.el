2d946e023ae54d980032dbeffeb03a2872cf2f11
Dillon Beliveau <dillonbeliveau@gmail.com>
2018-11-04 08:41:55 -0500

    Start work on new emacs config

---
+++ modules/emacs/fs/.emacs.d/init.el
@@ -0,0 +1,32 @@
+(tool-bar-mode -1) ; Hide toolbar
+(menu-bar-mode -1) ; Hide menubar
+(scroll-bar-mode -1) ; Hide scrollbar
+(global-linum-mode t) ; Line numbers
+
+; Add repos
+(require 'package)
+(add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/"))
+(add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
+(add-to-list 'package-archives '("melpa-stable" . "http://stable.melpa.org/packages/"))
+
+; Set up package management
+(setq package-enable-at-startup nil)
+(package-initialize)
+
+; Install use-package if it doesn't exist
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+(require 'use-package)
+
+(use-package evil
+  :ensure t
+  :init (evil-mode t))
+
+(use-package helm
+  :ensure t
+  :bind (("M-x" . helm-M-x)))
+
+(use-package monokai-theme
+  :ensure t
+  :init (load-theme 'monokai t))
