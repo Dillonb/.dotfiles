4f3a6283fe179c13c26f4d76024060d2e08c5a47
Dillon Beliveau <dillon.beliveau@coxautoinc.com>
2019-04-16 08:46:22 -0400

    Add rust-mode to emacs

--- modules/emacs/fs/.emacs.d/init.el
+++ modules/emacs/fs/.emacs.d/init.el
@@ -106,6 +106,9 @@
 (use-package yaml-mode
   :ensure t)
 
+(use-package rust-mode
+  :ensure t)
+
 (server-start)
 
 (setq-default c-default-style "k&r")
@@ -125,7 +128,7 @@
     ("527df6ab42b54d2e5f4eec8b091bd79b2fa9a1da38f5addd297d1c91aa19b616" "7559ac0083d1f08a46f65920303f970898a3d80f05905d01e81d49bb4c7f9e39" "c968804189e0fc963c641f5c9ad64bca431d41af2fb7e1d01a2a6666376f819c" default)))
  '(package-selected-packages
    (quote
-    (yaml-mode mediawiki-el mediawiki nix-mode lsp-java git-gutter+ markdown-mode git-gutter base16-theme terraform-mode evil-magit magit evil-leader use-package monokai-theme helm evil))))
+    (rust-mode yaml-mode mediawiki-el mediawiki nix-mode lsp-java git-gutter+ markdown-mode git-gutter base16-theme terraform-mode evil-magit magit evil-leader use-package monokai-theme helm evil))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
