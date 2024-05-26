279694f7152af9fec6ac5e20efd72a63bb57230c
Dillon Beliveau <dillonbeliveau@gmail.com>
2024-05-26 00:43:14 -0700

    remove comments

--- modules/nixos/modules/kde.nix
+++ modules/nixos/modules/kde.nix
@@ -3,9 +3,7 @@
   services.xserver.enable = true;
 
   # Enable the KDE Plasma Desktop Environment.
-  # services.xserver.displayManager.sddm.enable = true;
   services.displayManager.sddm.enable = true;
-  # services.xserver.desktopManager.plasma5.enable = true;
   services.desktopManager.plasma6.enable = true;
 
   # Configure keymap in X11
