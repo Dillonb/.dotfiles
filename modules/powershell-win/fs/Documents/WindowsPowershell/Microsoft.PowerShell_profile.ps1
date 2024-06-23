26e20f544bd67f2a7ae6a0aacc1dcc00f7ac11c7
Dillon Beliveau <dillonbeliveau@gmail.com>
2024-06-23 01:31:31 -0700

    Switch to oh-my-posh on Windows

--- modules/powershell-win/fs/Documents/WindowsPowershell/Microsoft.PowerShell_profile.ps1
+++ modules/powershell-win/fs/Documents/WindowsPowershell/Microsoft.PowerShell_profile.ps1
@@ -1,23 +1,8 @@
+if (Get-Command oh-my-posh) {
+  oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH\ys.omp.json" | Invoke-Expression
+} else {
+  Write-Host "Please install oh-my-posh from the Windows Store"
+}
+
 Set-PSReadlineKeyHandler -Key Tab -Function MenuComplete
 Set-PSReadLineOption -EditMode Emacs
-
-function global:prompt
-{
-    $hour = (Get-Date).Hour
-    $minute = (Get-Date).Minute
-    $second = (Get-Date).Second
-
-    $IsAdmin = (New-Object Security.Principal.WindowsPrincipal ([Security.Principal.WindowsIdentity]::GetCurrent())).IsInRole([Security.Principal.WindowsBuiltinRole]::Administrator)
-
-    Write-Host -Object "[${hour}:${minute}:${second}] " -NoNewLine -ForegroundColor Red
-    Write-Host -Object "${env:UserName}" -NoNewLine -ForegroundColor Green
-    Write-Host -Object "@" -NoNewLine
-    Write-Host -Object "${env:COMPUTERNAME}" -NoNewLine -ForegroundColor Blue
-    Write-Host -Object " $($executionContext.SessionState.Path.CurrentLocation)" -ForegroundColor Yellow
-
-    if ($IsAdmin) {
-        return "# "
-    } else {
-        return "$ "
-    }
-}
