f88d04c99cab2cf5c4ea5d066d70f4f8f40dacd8
Dillon Beliveau <dillonbeliveau@gmail.com>
2024-07-07 16:15:19 -0700

    Enable experimental features for nix-command and flakes

--- modules/nixos/configuration.nix
+++ modules/nixos/configuration.nix
@@ -46,6 +46,12 @@ in
     ./modules/libreoffice.nix
   ];
 
+  nix = {
+    settings = {
+      experimental-features = [ "nix-command" "flakes" ];
+    };
+  };
+
   # Configure network proxy if necessary
   # networking.proxy.default = "http://user:password@proxy:port/";
   # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
