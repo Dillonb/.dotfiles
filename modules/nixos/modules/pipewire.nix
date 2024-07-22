4d0014af45c5eef664421e96fc85f4bee87844a1
Dillon Beliveau <dillonbeliveau@gmail.com>
2024-07-22 00:21:04 -0700

    try pipewire again, this time with some extra config

--- modules/nixos/modules/pipewire.nix
+++ modules/nixos/modules/pipewire.nix
@@ -1,12 +1,37 @@
 {pkgs, ...}:
 {
   security.rtkit.enable = true;
+  hardware.pulseaudio.enable = false;
   services.pipewire = {
     enable = true;
     alsa.enable = true;
     alsa.support32Bit = true;
     pulse.enable = true;
     jack.enable = true;
+    # wireplumber.enable = true;
+    wireplumber = {
+      enable = true;
+      configPackages = [
+        # https://wiki.archlinux.org/title/PipeWire#Audio_cutting_out_when_multiple_streams_start_playing
+        (pkgs.writeTextDir "share/wireplumber/wireplumber.conf.d/50-alsa-config.conf" ''
+          monitor.alsa.rules = [
+            {
+              matches = [
+                {
+                  node.name = "~alsa_output.*"
+                }
+              ]
+              actions = {
+                update-props = {
+                  api.alsa.period-size = 1024
+                  api.alsa.headroom    = 8192
+                }
+              }
+            }
+          ]
+        '')
+      ];
+    };
 
     # Should fix audio crackling issues
     # TODO: different values for desktop and laptop?
