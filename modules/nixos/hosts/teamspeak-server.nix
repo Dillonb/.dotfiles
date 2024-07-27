bf46d7a82ff269e00e20a85abd2bea8aa5359dd4
Dillon Beliveau <dillonbeliveau@gmail.com>
2024-07-27 12:16:01 -0700

    this setting is in common

--- modules/nixos/hosts/teamspeak-server.nix
+++ modules/nixos/hosts/teamspeak-server.nix
@@ -4,7 +4,6 @@
   imports = [ "${modulesPath}/virtualisation/azure-common.nix" ];
 
   services.openssh.settings.PasswordAuthentication = false;
-  security.sudo.wheelNeedsPassword = false;
 
   system.stateVersion = "24.05";
 }
