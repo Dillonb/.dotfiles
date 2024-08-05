65a40dfeab3e5d3d4b76a126e28e7f0bd561f877
Dillon Beliveau <dillonbeliveau@gmail.com>
2024-08-04 17:13:33 -0700

    Add docker, nginx, firewall

--- modules/nixos/hosts/dulu/configuration.nix
+++ modules/nixos/hosts/dulu/configuration.nix
@@ -13,10 +13,85 @@
   # Use the systemd-boot EFI boot loader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
+
+  # ZFS
   boot.supportedFilesystems = [ "zfs" ];
   boot.zfs.extraPools = [ "zpool" ];
+
+  virtualisation.docker.enable = true;
+
   networking.hostId = "ad4d1b00";
-  system.stateVersion = "24.05"; # Did you read the comment?
+  system.stateVersion = "24.05";
+
+  networking.firewall = {
+    enable = true;
+    allowedTCPPorts = [ 22 80 443 9999 ];
+  };
+
+  services.nginx = {
+    enable = true;
+    virtualHosts = {
+      "dulu.dgb.sh" = {
+        forceSSL = true;
+        enableACME = true;
+        locations."/" = {
+          proxyPass = "http://localhost:32400/";
+        };
+      };
+
+      "jellyfin.dgb.sh" = {
+        forceSSL = true;
+        enableACME = true;
+        locations."/" = {
+          proxyPass = "http://localhost:8096/";
+        };
+
+        locations."/socket" = {
+          proxyWebsockets = true;
+        };
+      };
+
+      "r.cyphe.red" = {
+        forceSSL = true;
+        enableACME = true;
+        locations."/" = {
+          proxyPass = "http://localhost:7878/";
+        };
+      };
+
+      "s.cyphe.red" = {
+        forceSSL = true;
+        enableACME = true;
+        locations."/" = {
+          proxyPass = "http://localhost:8989/";
+        };
+      };
+
+      "nc.cyphe.red" = {
+        forceSSL = true;
+        enableACME = true;
+        locations."/" = {
+          proxyPass = "https://localhost:1010/";
+        };
+      };
+
+      "sab.cyphe.red" = {
+        forceSSL = true;
+        enableACME = true;
+        locations."/" = {
+          proxyPass = "http://localhost:8081/";
+        };
+      };
+
+      "t.cyphe.red" = {
+        forceSSL = true;
+        enableACME = true;
+        locations."/" = {
+          proxyPass = "http://localhost:9091/";
+        };
+      };
+    };
+  };
 
 }
 
