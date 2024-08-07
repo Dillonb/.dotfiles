f59cbeaaad3a916cf114cd514f9ce04e8ede94af
Dillon Beliveau <dillonbeliveau@gmail.com>
2024-08-06 20:23:51 -0700

    Add root disk to pi 4 config

--- modules/nixos/hosts/pi4.nix
+++ modules/nixos/hosts/pi4.nix
@@ -10,4 +10,9 @@
   nix.settings.trusted-users = [ "dillon" ];
 
   system.stateVersion = "24.05";
+
+  fileSystems."/" =
+    { device = "/dev/disk/by-uuid/44444444-4444-4444-8888-888888888888";
+      fsType = "ext4";
+    };
 }
