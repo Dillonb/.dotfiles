82f4d4f07130949a76467cb896323a8d07eeebee
Dillon Beliveau <dillonbeliveau@gmail.com>
2024-08-07 20:51:49 -0700

    Add systemd service for ble-scale

--- modules/nixos/hosts/pi4.nix
+++ modules/nixos/hosts/pi4.nix
@@ -1,4 +1,7 @@
-{ lib, ... }:
+{ pkgs, inputs, lib, ... }:
+let
+  ble-scale = inputs.ble-scale.packages."${pkgs.system}".default;
+in
 {
   boot.loader = {
     grub.enable = false;
@@ -12,6 +15,19 @@
   nix.settings.trusted-users = [ "dillon" ];
 
   system.stateVersion = "24.05";
+
+  systemd.services.ble-scale = {
+    wantedBy = [ "multi-user.target" ];
+    after = [ "network.target" ];
+    description = "BLE smart scale recording";
+    restartIfChanged = true;
+    serviceConfig = {
+      ExecStart = "${ble-scale}/bin/ble-scale -d /var/lib/syncthing-data/ble-scale-data/hook -s /var/lib/syncthing-data/ble-scale-data/sqlite.db";
+      User = "dillon";
+      Group = "users";
+      Restart = "on-failure";
+    };
+  };
 
   fileSystems."/" =
     { device = "/dev/disk/by-uuid/44444444-4444-4444-8888-888888888888";
