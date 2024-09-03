38aa7c4e8280487678c5150f8ce4ab24876711ce
Dillon Beliveau <dillonbeliveau@gmail.com>
2024-09-02 21:30:35 -0700

    hardware.opengl-enable -> hardware.graphics.enable

--- modules/nixos/hosts/mini.nix
+++ modules/nixos/hosts/mini.nix
@@ -47,7 +47,7 @@
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
-  hardware.opengl.enable = true;
+  hardware.graphics.enable = true;
 
   # Bootloader.
   boot.loader.systemd-boot.enable = true;
