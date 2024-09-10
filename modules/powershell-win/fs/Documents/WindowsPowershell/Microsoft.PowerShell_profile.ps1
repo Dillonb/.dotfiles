866df782c3f701fe5cfed13ee59021343902b2dc
Dillon Beliveau <dillonbeliveau@gmail.com>
2024-09-10 13:38:32 -0700

    Remove WindowsPowershell module

--- modules/powershell-win/fs/Documents/WindowsPowershell/Microsoft.PowerShell_profile.ps1
+++
@@ -1,8 +0,0 @@
-if (Get-Command oh-my-posh) {
-  oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH\ys.omp.json" | Invoke-Expression
-} else {
-  Write-Host "Please install oh-my-posh from the Windows Store"
-}
-
-Set-PSReadlineKeyHandler -Key Tab -Function MenuComplete
-Set-PSReadLineOption -EditMode Emacs
