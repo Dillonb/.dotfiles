4adc4c2b06da625bd958fbc26453fbb2ff91ae04
Dillon Beliveau <dillonbeliveau@gmail.com>
2024-09-11 21:59:00 -0700

    Error init.sh if chezmoi not available and no way to get it

--- init.sh
+++ init.sh
@@ -1,2 +1,14 @@
-chezmoi -S `readlink -f ~/.dotfiles/chezmoi` init
-chezmoi -S `readlink -f ~/.dotfiles/chezmoi` apply
+#!/usr/bin/env bash
+
+if command -v chezmoi > /dev/null; then
+  chezmoi -S `readlink -f ~/.dotfiles/chezmoi` init
+  chezmoi -S `readlink -f ~/.dotfiles/chezmoi` apply
+else
+  if command -v nix-shell > /dev/null; then
+    nix-shell -p chezmoi --command "chezmoi -S `readlink -f ~/.dotfiles/chezmoi` init"
+    nix-shell -p chezmoi --command "chezmoi -S `readlink -f ~/.dotfiles/chezmoi` apply"
+  else
+    echo "Please install chezmoi."
+    exit 1
+  fi
+fi
