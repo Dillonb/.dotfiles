9b1a8dd55cbaa6b3634e736c1f3f8f5b5a5d48e9
Dillon Beliveau <dillonbeliveau@gmail.com>
2024-09-20 00:46:51 -0700

    xdgOpenUsePortal = true

--- nix/workstation.nix
+++ nix/workstation.nix
@@ -87,6 +87,11 @@
     };
   };
 
+  xdg.portal = {
+    enable = true;
+    xdgOpenUsePortal = true;
+  };
+
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
   # on your system were taken. It‘s perfectly fine and recommended to leave
