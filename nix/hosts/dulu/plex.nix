587419e2ebd395f3b845d4fff51e29101e7e6707
Dillon Beliveau <dillonbeliveau@gmail.com>
2024-09-27 12:58:13 -0700

    Update plex server

--- nix/hosts/dulu/plex.nix
+++ nix/hosts/dulu/plex.nix
@@ -1,4 +1,4 @@
 {
-  version = "1.41.0.8992-8463ad060";
-  sha256 = "sha256-ldBJz2nqlzcx/FvKzMCgXkVO0omcojlU9sq6fAiknD8=";
+  version = "1.41.0.8994-f2c27da23";
+  sha256 = "sha256-e1COeawdR0pCF+qQ/xkTn/716iM9kB/fXom5MWHQ0YI=";
 }
