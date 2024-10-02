d6a1ab97a98d941184c2aabb27e5f2e0919984c0
Dillon Beliveau <dillonbeliveau@gmail.com>
2024-10-01 19:50:01 -0700

    Add Ombi

--- nix/hosts/dulu/nginx.nix
+++ nix/hosts/dulu/nginx.nix
@@ -1,4 +1,4 @@
-{ ... }:
+{ config, ... }:
 {
   services.nginx = {
     enable = true;
@@ -101,8 +101,35 @@
       "plex-requests.dgb.sh" = {
         forceSSL = true;
         enableACME = true;
+        extraConfig = ''
+          client_max_body_size 10m;
+          client_body_buffer_size 128k;
+
+          # Timeout if the real server is dead
+          proxy_next_upstream error timeout invalid_header http_500 http_502 http_503;
+
+          # Advanced Proxy Config
+          send_timeout 5m;
+          proxy_read_timeout 240;
+          proxy_send_timeout 240;
+          proxy_connect_timeout 240;
+
+          # Basic Proxy Config
+          proxy_set_header Host $host:$server_port;
+          proxy_set_header X-Real-IP $remote_addr;
+          proxy_set_header X-Forwarded-Host $server_name;
+          proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+          proxy_set_header X-Forwarded-Proto https;
+          proxy_redirect  http://  $scheme://;
+          proxy_http_version 1.1;
+          proxy_set_header Upgrade $http_upgrade;
+          proxy_set_header Connection "upgrade";
+          proxy_cache_bypass $cookie_session;
+          proxy_no_cache $cookie_session;
+          proxy_buffers 32 4k;
+        '';
         locations."/" = {
-          proxyPass = "http://127.0.0.1:3579/";
+          proxyPass = "http://127.0.0.1:${builtins.toString config.services.ombi.port}/";
         };
       };
 
