7b6fce56da3159406b5c83b120f1bb249746a2db
Dillon Beliveau <dillonbeliveau@gmail.com>
2024-10-16 23:44:59 -0700

    zigbee2mqtt and mosquitto

--- nix/hosts/dulu/configuration.nix
+++ nix/hosts/dulu/configuration.nix
@@ -39,7 +39,37 @@
 
   networking.firewall = {
     enable = true;
-    allowedTCPPorts = [ 22 80 443 8888 9999 ];
+    allowedTCPPorts = [ 22 80 443 8123 8888 9999 ];
+  };
+
+  services.mosquitto = {
+    enable = true;
+    listeners = [
+      {
+        acl = [ "pattern readwrite #" ];
+        omitPasswordAuth = true;
+        settings.allow_anonymous = true;
+      }
+    ];
+  };
+
+  services.zigbee2mqtt = {
+    enable = true;
+    settings = {
+      homeassistant = true;
+      permit_join = false;
+      serial = {
+        # port = "tcp://slzb-06m.local:6638";
+        port = "tcp://192.168.0.25:6638";
+        baudrate = 115200;
+        adapter = "ember";
+        disable_led = false;
+      };
+      advanced.transmit_power = 20;
+      mqtt = {
+        server = "mqtt://localhost:1883";
+      };
+    };
   };
 
   environment.systemPackages = with pkgs; [
