8d8d4b706313803552f93c9ec2e95a7521e5ea5b
Dillon Beliveau <dillonbeliveau@gmail.com>
2024-10-21 23:19:20 -0700

    Small updates

--- init.sh
+++ init.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
 if command -v chezmoi > /dev/null; then
-  chezmoi -S `readlink -f ~/.dotfiles/chezmoi` init
-  chezmoi -S `readlink -f ~/.dotfiles/chezmoi` apply
+  chezmoi -S "$(readlink -f ~/.dotfiles/chezmoi)" init
+  chezmoi -S "$(readlink -f ~/.dotfiles/chezmoi)" apply
 else
   if command -v nix-shell > /dev/null; then
-    nix-shell -p chezmoi --command "chezmoi -S `readlink -f ~/.dotfiles/chezmoi` init"
-    nix-shell -p chezmoi --command "chezmoi -S `readlink -f ~/.dotfiles/chezmoi` apply"
+    nix-shell -p chezmoi --command "chezmoi -S $(readlink -f ~/.dotfiles/chezmoi) init"
+    nix-shell -p chezmoi --command "chezmoi -S $(readlink -f ~/.dotfiles/chezmoi) apply"
   else
     echo "Please install chezmoi."
     exit 1
