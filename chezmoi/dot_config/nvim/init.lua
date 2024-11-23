9e8817e9897f072fcb4a2fdc0b3013625f1a1f3c
Dillon Beliveau <dillonbeliveau@gmail.com>
2024-11-23 11:53:21 -0500

    Formatting, dashboard, change session management plugin

--- chezmoi/dot_config/nvim/init.lua
+++ chezmoi/dot_config/nvim/init.lua
@@ -453,74 +453,118 @@ require("lazy").setup({
       end
     },
 
-    -- Automatic session save/restore
+    -- Automatic session saving + restore functionality
     {
-      'rmagatti/auto-session',
-      lazy = false,
-      dependencies = {
-        'nvim-telescope/telescope.nvim',
-      },
+      "folke/persistence.nvim",
+      event = "BufReadPre", -- this will only start session saving when an actual file was opened
+      opts = {
+        -- add any custom options here
+      }
+    },
+
+    -- HTTP Client
+    {
+      'mistweaverco/kulala.nvim',
+      opts = {},
       init = function()
-        require('auto-session').setup()
+        -- Associate .http files with this plugin
+        vim.filetype.add({
+          extension = {
+            ['http'] = 'http',
+          },
+        })
       end
     },
 
-  -- HTTP Client
-  {
-    'mistweaverco/kulala.nvim',
-    opts = {},
-    init = function()
-      -- Associate .http files with this plugin
-      vim.filetype.add({
-        extension = {
-          ['http'] = 'http',
-        },
-      })
-    end
-  },
-
-  -- Integration with tmux
-  {
-    "christoomey/vim-tmux-navigator",
-    cmd = {
-      "TmuxNavigateLeft",
-      "TmuxNavigateDown",
-      "TmuxNavigateUp",
-      "TmuxNavigateRight",
-      "TmuxNavigatePrevious",
+    -- Integration with tmux
+    {
+      "christoomey/vim-tmux-navigator",
+      cmd = {
+        "TmuxNavigateLeft",
+        "TmuxNavigateDown",
+        "TmuxNavigateUp",
+        "TmuxNavigateRight",
+        "TmuxNavigatePrevious",
+      },
+      keys = {
+        { "<c-h>", "<cmd><C-U>TmuxNavigateLeft<cr>" },
+        { "<c-j>", "<cmd><C-U>TmuxNavigateDown<cr>" },
+        { "<c-k>", "<cmd><C-U>TmuxNavigateUp<cr>" },
+        { "<c-l>", "<cmd><C-U>TmuxNavigateRight<cr>" },
+        { "<c-\\>", "<cmd><C-U>TmuxNavigatePrevious<cr>" },
+      },
     },
-    keys = {
-      { "<c-h>", "<cmd><C-U>TmuxNavigateLeft<cr>" },
-      { "<c-j>", "<cmd><C-U>TmuxNavigateDown<cr>" },
-      { "<c-k>", "<cmd><C-U>TmuxNavigateUp<cr>" },
-      { "<c-l>", "<cmd><C-U>TmuxNavigateRight<cr>" },
-      { "<c-\\>", "<cmd><C-U>TmuxNavigatePrevious<cr>" },
+
+    -- Changes made in a quickfix window are reflected in the actual file
+    {
+      "stefandtw/quickfix-reflector.vim"
     },
-  },
 
-  -- Changes made in a quickfix window are reflected in the actual file
-  {
-    "stefandtw/quickfix-reflector.vim"
-  },
+    -- Find unmapped keys easily
+    {
+      "meznaric/key-analyzer.nvim",
+      opts = {}
+    },
 
-  -- Find unmapped keys easily
-  {
-    "meznaric/key-analyzer.nvim",
-    opts = {}
-  },
+    -- Misc mini plugins
+    {
+      "folke/snacks.nvim",
+      priority = 1000,
+      lazy = false,
+      opts = {
+        quickfile = { enabled = true },
+        bigfile = { enabled = true },
+        statuscolumn = { enabled = true },
+        words = { enabled = true },
+        dashboard = {
+          enabled = true,
+          preset = {
+            keys = {
+              { icon = " ", key = "f", desc = "Find File", action = ":lua Snacks.dashboard.pick('files')" },
+              { icon = " ", key = "n", desc = "New File", action = ":ene | startinsert" },
+              { icon = " ", key = "g", desc = "Find Text", action = ":lua Snacks.dashboard.pick('live_grep')" },
+              { icon = " ", key = "r", desc = "Recent Files", action = ":lua Snacks.dashboard.pick('oldfiles')" },
+              { icon = " ", key = "c", desc = "Config", action = ":lua Snacks.dashboard.pick('files', {cwd = vim.fn.stdpath('config')})" },
+              { icon = " ", key = "s", desc = "Restore Current Dir Session", section = "session" },
+              { icon = "󰒲 ", key = "L", desc = "Lazy", action = ":Lazy", enabled = package.loaded.lazy ~= nil },
+              { icon = " ", key = "q", desc = "Quit", action = ":qa" },
+            },
+          },
+          sections = {
+            { section = "header" },
+            -- {
+            --   pane = 2,
+            --   section = "terminal",
+            --   cmd = "colorscript -e square",
+            --   height = 5,
+            --   padding = 1,
+            -- },
+            { section = "keys", gap = 1, padding = 1 },
+            { pane = 2, icon = " ", title = "Recent Files", section = "recent_files", indent = 2, padding = 1 },
+            { pane = 2, icon = " ", title = "Projects", section = "projects", indent = 2, padding = 1 },
+            -- {
+            --   pane = 2,
+            --   icon = " ",
+            --   title = "Git Status",
+            --   section = "terminal",
+            --   -- enabled = require('snacks').git.get_root() ~= nil,
+            --   enabled = true,
+            --   cmd = "hub status --short --branch --renames",
+            --   height = 5,
+            --   padding = 1,
+            --   ttl = 5 * 60,
+            --   indent = 3,
+            -- },
+            { section = "startup" },
+          },
+        }
+      }
+    },
 
-  -- Misc mini plugins
-  {
-    "folke/snacks.nvim",
-    priority = 1000,
-    lazy = false,
-    opts = {
-      quickfile = { enabled = true },
-      bigfile = { enabled = true },
-      statuscolumn = { enabled = true },
-      words = { enabled = true }
+    -- Nicer UI for selections
+    {
+      "stevearc/dressing.nvim"
     }
-  }
 })
 
 vim.api.nvim_create_autocmd("LspAttach", {
