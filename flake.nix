dd51675896a8ed4c873fd1379dc307606db8367e
Dillon Beliveau <dillonbeliveau@gmail.com>
2024-12-22 21:07:44 -0800

    nh on Darwin from the beta flake

--- flake.nix
+++ flake.nix
@@ -17,6 +17,11 @@
       inputs.nixpkgs.follows = "nixpkgs-unstable";
     };
 
+    nh = {
+      url = "github:viperML/nh";
+      inputs.nixpkgs.follows = "nixpkgs-unstable";
+    };
+
     home-manager-stable = {
       url = "github:nix-community/home-manager/release-24.11";
       inputs.nixpkgs.follows = "nixpkgs-stable";
