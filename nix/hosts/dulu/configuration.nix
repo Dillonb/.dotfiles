4853b98de630eb193e86b93494347b1732ede642
Dillon Beliveau <dillonbeliveau@gmail.com>
2025-01-23 21:52:44 -0800

    Enable avahi on dulu

--- nix/hosts/dulu/configuration.nix
+++ nix/hosts/dulu/configuration.nix
@@ -96,6 +96,18 @@
     };
   };
 
+  services.avahi = {
+    enable = true;
+    nssmdns4 = true;
+    nssmdns6 = true;
+    publish = {
+      enable = true;
+      addresses = true;
+      domain = true;
+      hinfo = true;
+      userServices = true;
+    };
+  };
 
 }
 
