2017630221911389cd9b6b5dbfe2916a66831e0b
Dillon Beliveau <dillon@dgb.sh>
2025-02-10 02:09:31 -0800

    Remove unnecessary open ports

--- nix/hosts/dulu/configuration.nix
+++ nix/hosts/dulu/configuration.nix
@@ -40,7 +40,7 @@
 
   networking.firewall = {
     enable = true;
-    allowedTCPPorts = [ 22 80 443 8888 9999 ];
+    allowedTCPPorts = [ 22 80 443 ];
   };
 
   environment.systemPackages = with pkgs; [
