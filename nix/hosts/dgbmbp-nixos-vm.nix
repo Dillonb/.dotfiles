c5fa7582ff17e98c2d6e0b228f29674da5f72650
Dillon Beliveau <dillonbeliveau@gmail.com>
2025-02-15 15:34:48 -0800

    Add dgbmbp-nixos-vm host

---
+++ nix/hosts/dgbmbp-nixos-vm.nix
@@ -0,0 +1,17 @@
+{ ... }:
+{
+  fileSystems."/" =
+    {
+      device = "/dev/disk/by-uuid/54623f08-da1c-46fa-a9e8-9faf110bfd86";
+      fsType = "ext4";
+    };
+
+  fileSystems."/boot" =
+    {
+      device = "/dev/disk/by-uuid/143A-D0E1";
+      fsType = "vfat";
+      options = [ "fmask=0077" "dmask=0077" ];
+    };
+
+  virtualisation.vmware.guest.enable = true;
+}
