64d9f3e29ed6487321166b1246fdce75925b1de5
Dillon Beliveau <dillonbeliveau@gmail.com>
2025-02-16 18:31:03 -0800

    Disable kbfs

--- nix/workstation.nix
+++ nix/workstation.nix
@@ -88,7 +88,6 @@
 
   # Services
   services = {
-    kbfs.enable = true;
     # "discouraged" to turn this off, but Steam downloads are very slow with this on.
     # nscd.enableNsncd = false;
     resolved.enable = true;
