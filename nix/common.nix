e5b4daa5a7c6312700163b02174bab436187dcac
Dillon Beliveau <dillonbeliveau@gmail.com>
2025-03-16 23:59:48 -0700

    Add n64-tools.cachix.org to substituters

--- nix/common.nix
+++ nix/common.nix
@@ -26,12 +26,13 @@
       substituters = [
         "https://cache.nix.dgb.sh"
         "https://nix-community.cachix.org"
-        "https://cache.nixos.org/"
+        "https://n64-tools.cachix.org"
       ];
 
       trusted-public-keys = [
         "cache.nix.dgb.sh:XzJS7VYoZ90T3fwKySBAqfKdxViX6zk2PJvlKVD+euU="
         "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+        "n64-tools.cachix.org-1:LsRfMtKGDYgYXKLOJOPz7ktRups5k+X2LxbGzVX3K6A="
       ];
     };
     # package = pkgs.lix;
