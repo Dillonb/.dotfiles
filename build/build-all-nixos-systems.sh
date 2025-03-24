1913e45aa7d6359b4a7f574dc9539c5e82214514
Dillon Beliveau <dillon@dgb.sh>
2025-03-23 18:13:07 -0700

    Multicore builds, set -e

--- build/build-all-nixos-systems.sh
+++ build/build-all-nixos-systems.sh
@@ -1,9 +1,7 @@
 #!/usr/bin/env bash
-#! nix-shell -i toilet jq
-
+set -e
 
 # Generate the JSON list of all derivations
-# nix build --cores 1 ..#packages.x86_64-linux.all-nixos-systems --out-link all-nixos-systems.json
 nix build ..#packages.x86_64-linux.all-nixos-systems --out-link all-nixos-systems.json
 cat all-nixos-systems.json
 
@@ -11,5 +9,5 @@ mkdir -p systems-gcroots
 
 jq -r -c '.[]' all-nixos-systems.json | while read -r i; do
     toilet -f future "$i"
-    nix build --cores 1 ..#nixosConfigurations."$i".config.system.build.toplevel --out-link systems-gcroots/"$i"
+    nix build ..#nixosConfigurations."$i".config.system.build.toplevel --out-link systems-gcroots/"$i"
 done
