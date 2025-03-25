f4caba3542a786013957123e87cb4bef3dd6d7c6
Dillon Beliveau <dillon@dgb.sh>
2025-03-24 22:05:16 -0700

    Use separate script for ci

--- build/build-all-nixos-systems.sh
+++ build/build-all-nixos-systems.sh
@@ -1,13 +1,4 @@
 #!/usr/bin/env nix-shell
 #! nix-shell -i bash -p jq toilet
 
-# Generate the JSON list of all derivations
-nix build ..#packages.x86_64-linux.all-nixos-systems --out-link all-nixos-systems.json
-cat all-nixos-systems.json
-
-mkdir -p systems-gcroots
-
-jq -r -c '.[]' all-nixos-systems.json | while read -r i; do
-    toilet -f future "$i"
-    nix build --cores 1 ..#nixosConfigurations."$i".config.system.build.toplevel --out-link systems-gcroots/"$i"
-done
+./ci-build-all-nixos-systems.sh
---
+++ build/ci-build-all-nixos-systems.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+# Generate the JSON list of all derivations
+nix build ..#packages.x86_64-linux.all-nixos-systems --out-link all-nixos-systems.json
+cat all-nixos-systems.json
+
+mkdir -p systems-gcroots
+
+jq -r -c '.[]' all-nixos-systems.json | while read -r i; do
+    toilet -f future "$i"
+    nix build --cores 1 ..#nixosConfigurations."$i".config.system.build.toplevel --out-link systems-gcroots/"$i"
+done
