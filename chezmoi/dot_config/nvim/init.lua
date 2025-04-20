4e34f377bff579b5aa9b5023cbc622b6fdea6536
Dillon Beliveau <dillon@dgb.sh>
2025-04-20 14:00:42 -0700

    Add missing luasnip dependency

--- chezmoi/dot_config/nvim/init.lua
+++ chezmoi/dot_config/nvim/init.lua
@@ -176,6 +176,7 @@ require("lazy").setup({
           "hrsh7th/cmp-buffer",
           "hrsh7th/cmp-path",
           "hrsh7th/cmp-cmdline",
+          "L3MON4D3/LuaSnip",
           "saadparwaiz1/cmp_luasnip",
         }
       },
