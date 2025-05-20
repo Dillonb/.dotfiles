5da95f4e839e076f298e49c91acb223e489242a6
Dillon Beliveau <dillon@dgb.sh>
2025-05-20 10:36:46 -0700

    Update slzb-06m local ip address

--- nix/hosts/dulu/smart-home.nix
+++ nix/hosts/dulu/smart-home.nix
@@ -16,10 +16,6 @@
     ];
   };
 
-  # TODO: remove this next line when I upgrade to 25.04
-  # See: https://github.com/NixOS/nixpkgs/commit/99eabc0ab3dab1634eadc0092fb2efad8321d9c3#diff-ae914e83933020486ee71b87a797531d86925ecb8d2d4544b028b23e2fc2c8b7
-  systemd.services.zigbee2mqtt.serviceConfig.SystemCallFilter = [ "@chown" ];
-
   services.zigbee2mqtt = {
     enable = true;
     package = pkgs.unstable.zigbee2mqtt_2;
@@ -32,8 +28,7 @@
         # port = "mdns://slzb-06";
 
         # $ avahi-resolve --name slzb-06m.local
-        port = "tcp://192.168.0.43:6638";
-        # port = "tcp://192.168.0.25:6638";
+        port = "tcp://192.168.1.204:6638";
 
         baudrate = 115200;
         adapter = "ember";
