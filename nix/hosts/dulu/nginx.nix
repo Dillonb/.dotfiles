97386657ae4b5c2ee52479ed5fe288da5052ec28
Dillon Beliveau <dillon@dgb.sh>
2025-05-24 00:25:07 -0700

    Fix JellyFin websockets

--- nix/hosts/dulu/nginx.nix
+++ nix/hosts/dulu/nginx.nix
@@ -135,10 +135,6 @@
         enableACME = true;
         locations."/" = {
           proxyPass = "http://127.0.0.1:8096/";
-        };
-
-        locations."/socket" = {
-          proxyPass = "http://127.0.0.1:8096/";
           proxyWebsockets = true;
         };
       };
