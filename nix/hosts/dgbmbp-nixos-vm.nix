dd11d548bc1facab16c42748eea20263dac70600
Dillon Beliveau <dillon@dgb.sh>
2025-06-07 15:05:22 -0700

    Update VM disk UUIDs

--- nix/hosts/dgbmbp-nixos-vm.nix
+++ nix/hosts/dgbmbp-nixos-vm.nix
@@ -2,13 +2,13 @@
 {
   fileSystems."/" =
     {
-      device = "/dev/disk/by-uuid/54623f08-da1c-46fa-a9e8-9faf110bfd86";
+      device = "/dev/disk/by-uuid/d5e9bfe4-c922-4dc0-bad9-b9a58b54bd6e";
       fsType = "ext4";
     };
 
   fileSystems."/boot" =
     {
-      device = "/dev/disk/by-uuid/143A-D0E1";
+      device = "/dev/disk/by-uuid/D23F-4093";
       fsType = "vfat";
       options = [ "fmask=0077" "dmask=0077" ];
     };
