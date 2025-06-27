21466e66bb05aa994bcfb0652752b4e17e0793d0
Dillon Beliveau <dillon@dgb.sh>
2025-06-27 00:50:01 -0700

    add conform for formatting

--- chezmoi/dot_config/nvim/init.lua
+++ chezmoi/dot_config/nvim/init.lua
@@ -775,6 +775,32 @@ require("lazy").setup({
         }
       })
     end
+  },
+  {
+    'stevearc/conform.nvim',
+    event = { 'BufWritePre' },
+    cmd = { "ConformInfo" },
+    keys = {
+      {
+        '<leader>fb',
+        function()
+          require("conform").format({ async = true })
+        end,
+        mode = "",
+        desc = "Format buffer"
+      }
+    },
+    opts = {
+      formatters_by_ft = {
+        rust = { "rustfmt" },
+      },
+      default_format_opts = {
+        lsp_format = "fallback",
+      },
+    },
+    init = function()
+      vim.o.formatexpr = "v:lua.require'conform'.formatexpr()"
+    end,
   }
 })
 
