488886f0f9b524bad2d02a135a76d8f98f11c43d
Dillon Beliveau <dillon@dgb.sh>
2025-11-25 09:05:20 -0800

    Update Plex

--- nix/hosts/dulu/plex.nix
+++ nix/hosts/dulu/plex.nix
@@ -1,4 +1,4 @@
 {
-  version = "1.42.2.10122-3749b980e";
-  sha256 = "sha256-9XIBRNSlLeA1YENSjGo2cM5n70xXrkw+d4q7lpqD7RY=";
+  version = "1.43.0.10346-fc911a729";
+  sha256 = "sha256-jfQ11luQoafUYb5sGpvE7jPiErGt2HXVLwn70M/Hqyc=";
 }
