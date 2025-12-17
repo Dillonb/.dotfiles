332898a20962f9bbaeb8070b2be477a4e97b4d8f
Dillon Beliveau <dillon@dgb.sh>
2025-12-16 21:33:43 -0800

    Fix libvirtd, add virt-manager

--- nix/hosts/battlestation.nix
+++ nix/hosts/battlestation.nix
@@ -123,14 +123,18 @@ in
   };
 
   # libvirtd
-  virtualisation.libvirtd = {
-    enable = true;
-    qemu = {
-      package = pkgs.qemu_kvm;
-      runAsRoot = true;
-      swtpm.enable = true;
+  virtualisation = {
+    spiceUSBRedirection.enable = true;
+    libvirtd = {
+      enable = true;
+      qemu = {
+        package = pkgs.qemu_kvm;
+        runAsRoot = true;
+        swtpm.enable = true;
+      };
     };
   };
+  programs.virt-manager.enable = true;
 
   services.restic = {
     backups = {
