5df11ba3f2258bc7860a25c97850c60145a11f52
Dillon Beliveau <dillon@dgb.sh>
2026-01-26 10:21:12 -0800

    Update copyparty

--- flake.nix
+++ flake.nix
@@ -50,7 +50,7 @@
       inputs.nixpkgs.follows = "nixos-unstable";
     };
     copyparty = {
-      url = "github:9001/copyparty/v1.20.2";
+      url = "github:9001/copyparty/v1.20.4";
       inputs.nixpkgs.follows = "nixos-unstable";
     };
   };
