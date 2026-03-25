819ef9ae66921d6eb97d616d5cef7db75af07bbd
Dillon Beliveau <dillon@dgb.sh>
2026-03-24 21:47:55 -0700

    Update nvidia driver

--- nix/hosts/battlestation.nix
+++ nix/hosts/battlestation.nix
@@ -8,13 +8,13 @@
 }:
 let
   # nvidia-driver = config.boot.kernelPackages.nvidiaPackages.latest;
-  nvidia_595_45_04 = config.boot.kernelPackages.nvidiaPackages.mkDriver {
-    version = "595.45.04";
-    sha256_64bit = "sha256-zUllSSRsuio7dSkcbBTuxF+dN12d6jEPE0WgGvVOj14=";
-    sha256_aarch64 = "sha256-jl6lQWsgF6ya22sAhYPpERJ9r+wjnWzbGnINDpUMzsk=";
-    openSha256 = "sha256-uqNfImwTKhK8gncUdP1TPp0D6Gog4MSeIJMZQiJWDoE=";
-    settingsSha256 = "sha256-Y45pryyM+6ZTJyRaRF3LMKaiIWxB5gF5gGEEcQVr9nA=";
-    persistencedSha256 = "sha256-5FoeUaRRMBIPEWGy4Uo0Aho39KXmjzQsuAD9m/XkNpA=";
+  nvidia_595_58_03 = config.boot.kernelPackages.nvidiaPackages.mkDriver {
+    version = "595.58.03";
+    sha256_64bit = "sha256-jA1Plnt5MsSrVxQnKu6BAzkrCnAskq+lVRdtNiBYKfk=";
+    sha256_aarch64 = "sha256-hzzIKY1Te8QkCBWR+H5k1FB/HK1UgGhai6cl3wEaPT8=";
+    openSha256 = "sha256-6LvJyT0cMXGS290Dh8hd9rc+nYZqBzDIlItOFk8S4n8=";
+    settingsSha256 = "sha256-2vLF5Evl2D6tRQJo0uUyY3tpWqjvJQ0/Rpxan3NOD3c=";
+    persistencedSha256 = "sha256-AtjM/ml/ngZil8DMYNH+P111ohuk9mWw5t4z7CHjPWw=";
   };
 in
 
@@ -98,7 +98,7 @@ in
     modesetting.enable = true;
     open = false;
     nvidiaSettings = true;
-    package = nvidia_595_45_04;
+    package = nvidia_595_58_03;
   };
 
   boot.kernelParams = [
