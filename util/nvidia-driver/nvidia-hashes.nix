b822e5c70657a07897363397f4b8e2ce1cd31dde
Dillon Beliveau <dillon@dgb.sh>
2026-03-26 11:05:04 -0700

    Nix fmt

--- util/nvidia-driver/nvidia-hashes.nix
+++ util/nvidia-driver/nvidia-hashes.nix
@@ -1,7 +1,8 @@
 { version }:
 let
   # File hash (for .run driver binaries fetched with nixpkgs fetchurl)
-  fetchFileHash = url:
+  fetchFileHash =
+    url:
     builtins.convertHash {
       hash = builtins.hashFile "sha256" (builtins.fetchurl url);
       hashAlgo = "sha256";
@@ -9,31 +10,30 @@ let
     };
 
   # NAR hash of unpacked tarball (for GitHub archives fetched with fetchFromGitHub/fetchzip)
-  fetchTreeHash = url:
-    (builtins.fetchTree { type = "tarball"; inherit url; }).narHash;
+  fetchTreeHash =
+    url:
+    (builtins.fetchTree {
+      type = "tarball";
+      inherit url;
+    }).narHash;
 
-  sha256_64bit = fetchFileHash
-    "https://us.download.nvidia.com/XFree86/Linux-x86_64/${version}/NVIDIA-Linux-x86_64-${version}.run";
+  sha256_64bit = fetchFileHash "https://us.download.nvidia.com/XFree86/Linux-x86_64/${version}/NVIDIA-Linux-x86_64-${version}.run";
 
-  sha256_aarch64 = fetchFileHash
-    "https://us.download.nvidia.com/XFree86/aarch64/${version}/NVIDIA-Linux-aarch64-${version}.run";
+  sha256_aarch64 = fetchFileHash "https://us.download.nvidia.com/XFree86/aarch64/${version}/NVIDIA-Linux-aarch64-${version}.run";
 
-  openSha256 = fetchTreeHash
-    "https://github.com/NVIDIA/open-gpu-kernel-modules/archive/${version}.tar.gz";
+  openSha256 = fetchTreeHash "https://github.com/NVIDIA/open-gpu-kernel-modules/archive/${version}.tar.gz";
 
-  settingsSha256 = fetchTreeHash
-    "https://github.com/NVIDIA/nvidia-settings/archive/${version}.tar.gz";
+  settingsSha256 = fetchTreeHash "https://github.com/NVIDIA/nvidia-settings/archive/${version}.tar.gz";
 
-  persistencedSha256 = fetchTreeHash
-    "https://github.com/NVIDIA/nvidia-persistenced/archive/${version}.tar.gz";
+  persistencedSha256 = fetchTreeHash "https://github.com/NVIDIA/nvidia-persistenced/archive/${version}.tar.gz";
 in
 ''
-config.boot.kernelPackages.nvidiaPackages.mkDriver {
-  version = "${version}";
-  sha256_64bit = "${sha256_64bit}";
-  sha256_aarch64 = "${sha256_aarch64}";
-  openSha256 = "${openSha256}";
-  settingsSha256 = "${settingsSha256}";
-  persistencedSha256 = "${persistencedSha256}";
-}
+  config.boot.kernelPackages.nvidiaPackages.mkDriver {
+    version = "${version}";
+    sha256_64bit = "${sha256_64bit}";
+    sha256_aarch64 = "${sha256_aarch64}";
+    openSha256 = "${openSha256}";
+    settingsSha256 = "${settingsSha256}";
+    persistencedSha256 = "${persistencedSha256}";
+  }
 ''
