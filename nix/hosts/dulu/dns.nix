99ff540d9a6a5241f3bc3811a333c8110ff80e6c
Dillon Beliveau <dillon@dgb.sh>
2026-04-06 00:01:46 -0700

    Fix ddclient

--- nix/hosts/dulu/dns.nix
+++ nix/hosts/dulu/dns.nix
@@ -4,7 +4,7 @@
     enable = true;
     protocol = "namecheap";
     username = "dgb.sh";
-    usev4 = "web,web=ifconfig.me/ip";
+    usev4 = "webv4, webv4=ipify-ipv4";
     passwordFile = config.age.secrets."dgb.sh-dynamic-dns-password".path;
     domains = [ "@" ];
   };
