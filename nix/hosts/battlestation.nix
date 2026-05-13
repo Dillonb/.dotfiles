5bd3f47473ced74adf8981ca3b9f7e5bb6dcea0b
Dillon Beliveau <dillon@dgb.sh>
2026-05-12 23:53:19 -0700

    Back to nvidiaPackages.latest

--- nix/hosts/battlestation.nix
+++ nix/hosts/battlestation.nix
@@ -7,15 +7,7 @@
   ...
 }:
 let
-  # nvidia-driver = config.boot.kernelPackages.nvidiaPackages.latest;
-  nvidia_595_58_03 = config.boot.kernelPackages.nvidiaPackages.mkDriver {
-    version = "595.58.03";
-    sha256_64bit = "sha256-jA1Plnt5MsSrVxQnKu6BAzkrCnAskq+lVRdtNiBYKfk=";
-    sha256_aarch64 = "sha256-hzzIKY1Te8QkCBWR+H5k1FB/HK1UgGhai6cl3wEaPT8=";
-    openSha256 = "sha256-6LvJyT0cMXGS290Dh8hd9rc+nYZqBzDIlItOFk8S4n8=";
-    settingsSha256 = "sha256-2vLF5Evl2D6tRQJo0uUyY3tpWqjvJQ0/Rpxan3NOD3c=";
-    persistencedSha256 = "sha256-AtjM/ml/ngZil8DMYNH+P111ohuk9mWw5t4z7CHjPWw=";
-  };
+  nvidia-driver = config.boot.kernelPackages.nvidiaPackages.latest;
 in
 
 {
@@ -98,7 +90,7 @@ in
     modesetting.enable = true;
     open = false;
     nvidiaSettings = true;
-    package = nvidia_595_58_03;
+    package = nvidia-driver;
   };
 
   boot.kernelParams = [
