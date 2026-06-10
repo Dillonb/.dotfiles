c98f0fac7b09f5ae67e35e6a804d8978bb642d92
Dillon Beliveau <dillon@dgb.sh>
2026-06-10 14:56:55 -0700

    Alert on netdata when systemd units fail

--- nix/modules/netdata.nix
+++ nix/modules/netdata.nix
@@ -1,4 +1,41 @@
-{ config, ... }:
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+let
+  # Alert when systemd units fail
+  unitTypes = [
+    "service"
+    "socket"
+    "target"
+    "path"
+    "device"
+    "mount"
+    "automount"
+    "swap"
+    "scope"
+    "slice"
+    "timer"
+  ];
+  unitAlert = type: ''
+        template: systemd_${type}_unit_failed_state
+              on: systemd.${type}_unit_state
+           class: Errors
+            type: Linux
+       component: Systemd units
+    chart labels: unit_name=*
+            calc: $failed
+           units: state
+           every: 10s
+            warn: $this != nan AND $this == 1
+           delay: down 5m multiplier 1.5 max 1h
+         summary: systemd ${type} ''${label:unit_name} failed
+            info: systemd ${type} unit is in the failed state
+              to: sysadmin
+  '';
+in
 {
   users.users.netdata.extraGroups = [
     "agenix" # secrets access
@@ -7,6 +44,9 @@
     enable = true;
     configDir = {
       "health_alarm_notify.conf" = config.age.secrets."netdata-discord.conf".path;
+      "health.d/systemdunits.conf" = pkgs.writeText "systemdunits-health.conf" (
+        lib.concatMapStringsSep "\n" unitAlert unitTypes
+      );
     };
   };
 }
