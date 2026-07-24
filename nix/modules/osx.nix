7dcff2fc2f03554672396481940f74f5c5e57374
Dillon Beliveau <dillon@dgb.sh>
2026-07-24 15:36:21 -0700

    Add substituters to osx

--- nix/modules/osx.nix
+++ nix/modules/osx.nix
@@ -18,6 +18,20 @@ in
       # This casues problems on osx, so disable it
       # auto-optimise-store = true;
       trusted-users = [ "${dgbCustom.username}" ];
+
+      substituters = [
+        "https://cache.nix.dgb.sh"
+        "https://attic.dgb.sh/system"
+        "https://nix-community.cachix.org"
+        "https://n64-tools.cachix.org"
+      ];
+
+      trusted-public-keys = [
+        "cache.nix.dgb.sh:XzJS7VYoZ90T3fwKySBAqfKdxViX6zk2PJvlKVD+euU="
+        "system:E5ubGe/xsSF5nf2gxvZBP4WSVVFWis3lo+3A3HERoxc="
+        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+        "n64-tools.cachix.org-1:LsRfMtKGDYgYXKLOJOPz7ktRups5k+X2LxbGzVX3K6A="
+      ];
     };
   };
 
