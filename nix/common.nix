a1352beda6bc3f32e9ff070a5507800f2ffcfed2
Dillon Beliveau <dillon@dgb.sh>
2026-07-25 18:03:25 -0700

    Switch to vimscript syntax, oops

--- nix/common.nix
+++ nix/common.nix
@@ -81,7 +81,7 @@
       configure = {
         customRC = ''
           " Tell nvim where GCC is. Needed so it can compile the treesitter parsers.
-          vim.env.CC = '${pkgs.lib.getExe pkgs.gcc}'
+          let $CC = '${pkgs.lib.getExe pkgs.gcc}'
 
           " Set a variable so we know we're in NixOS
           let g:os_is_nixos = v:true
