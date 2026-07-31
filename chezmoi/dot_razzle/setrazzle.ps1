8516374e9dc5e9ecd51e8bfe6e7a9d48b597bab9
Dillon Beliveau <dillon@dgb.sh>
2026-07-30 17:55:29 -0700

    Fix

--- chezmoi/dot_razzle/setrazzle.ps1
+++ chezmoi/dot_razzle/setrazzle.ps1
@@ -1,7 +1,8 @@
 if (Get-Command zoxide -ErrorAction SilentlyContinue) {
-  # This is set to 1 by my regular profile, then Razzle overrides the zoxide hook.
-  # Set it to 0 to force it to actually reinitialize here
-  $global:__zoxide_hooked = 0
+  # Skip if zoxide's wrapper is installed already (avoid infinite recursion)
+  if (${function:prompt} -notmatch '__zoxide_hook') {
+    $global:__zoxide_hooked = 0
 
-  Invoke-Expression (& { (zoxide init powershell | Out-String) })
+    Invoke-Expression (& { (zoxide init powershell | Out-String) })
+  }
 }
