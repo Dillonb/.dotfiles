c3904f01f4d87a82687712d74c75ffd8cad83758
Dillon Beliveau <dbeliveau@xbox.com>
2026-08-21 11:05:28 -0700

    Manually set DNS for WSL

--- nix/hosts/wsl/configuration.nix
+++ nix/hosts/wsl/configuration.nix
@@ -7,4 +7,12 @@
 
 {
   system.stateVersion = "24.05"; # Did you read the comment?
+
+  wsl.wslConf.network.generateResolvConf = false;
+  networking.nameservers = [
+    "192.168.1.1"
+    "1.1.1.1"
+    "8.8.8.8"
+    "8.8.4.4"
+  ];
 }
