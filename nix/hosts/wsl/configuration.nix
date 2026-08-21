cbf7b4f22c2ace7b43a31116d4af45c35b7c8a40
Dillon Beliveau <dillon@wsl.localdomain>
2026-08-21 11:25:08 -0700

    Remove 192.168.1.1 from dns on wsl

--- nix/hosts/wsl/configuration.nix
+++ nix/hosts/wsl/configuration.nix
@@ -1,16 +1,10 @@
-{
-  config,
-  lib,
-  pkgs,
-  ...
-}:
+{ ... }:
 
 {
   system.stateVersion = "24.05"; # Did you read the comment?
 
   wsl.wslConf.network.generateResolvConf = false;
   networking.nameservers = [
-    "192.168.1.1"
     "1.1.1.1"
     "8.8.8.8"
     "8.8.4.4"
