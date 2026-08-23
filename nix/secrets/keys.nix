b595f5c863c1a16d3893baf3fec25aabfe541ede
Dillon Beliveau <dillon@dgb.sh>
2026-08-23 13:44:15 -0700

    Add bc-250 keys

--- nix/secrets/keys.nix
+++ nix/secrets/keys.nix
@@ -9,11 +9,13 @@
     "pi4" = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILOeEvZymnFYVFnwWj1a/ys2e4yGaOH9S/fLblbrJw7Z";
     "dgbmbp-nixos-vm" =
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAmfX8OldvzJt2ia6yVFdjtmA0i7hjw4XDFeKoTgluPJ";
+    "bc250" = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINz38cAw97vTsR3HC7dlossfARztObclE+khcOPFWKyT";
   };
   # ~/.ssh/id_ed25519.pub
   user = {
     "battlestation" =
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMNsw5AFJSHDB11PdcOY60UeIGIhj3MRfBRLYrElHOL1";
     "dulu" = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFxQ3M2CH8EHwat4DEypSt6GSeIwSEpZCJxJnPso6ve/";
+    "bc250" = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGxBH4YU3aJlJKmUA6OFhaFeKoSZHPJT4711A2H4WEaR";
   };
 }
