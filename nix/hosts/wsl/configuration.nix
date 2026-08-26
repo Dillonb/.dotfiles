47242227c51a341964ae4b19acb083cf382bc0c8
Dillon Beliveau <dillon@dgb.sh>
2026-08-25 17:20:49 -0700

    generate a resolv conf

--- nix/hosts/wsl/configuration.nix
+++ nix/hosts/wsl/configuration.nix
@@ -3,10 +3,5 @@
 {
   system.stateVersion = "24.05"; # Did you read the comment?
 
-  wsl.wslConf.network.generateResolvConf = false;
-  networking.nameservers = [
-    "1.1.1.1"
-    "8.8.8.8"
-    "8.8.4.4"
-  ];
+  wsl.wslConf.network.generateResolvConf = true;
 }
