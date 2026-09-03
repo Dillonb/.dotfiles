25d44035d5d6ee0dbf37ea665d11335e4450bf4d
Dillon Beliveau <dillon@dgb.sh>
2026-09-02 22:52:21 -0700

    Enable cu unlock

--- nix/hosts/bc250/configuration.nix
+++ nix/hosts/bc250/configuration.nix
@@ -20,8 +20,7 @@ in
 
       # Only needed for AIC8800D80-based WiFi dongles.
       aic8800d80.enable = false;
-      # Test stability of the CU unlock before enabling.
-      cuLiveManager.enable = false;
+      cuLiveManager.enable = true;
       # Requires running `bc250-detect` first and setting cpuOverclock.configFile.
       cpuOverclock.enable = false;
       # Modded BIOSes may already provide these; enabling both conflicts.
