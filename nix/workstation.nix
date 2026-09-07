33099404fee3078dd94ea1b7c6e7265a6de3e43c
Dillon Beliveau <dillon@dgb.sh>
2026-09-06 22:55:54 -0700

    Add icu to steam extraPackages

--- nix/workstation.nix
+++ nix/workstation.nix
@@ -64,7 +64,10 @@
       extraCompatPackages = with pkgs; [ proton-ge-bin ];
 
       # Because steam-run is the best way to get unusual binaries to run
-      extraPackages = with pkgs; [ libxml2 ];
+      extraPackages = with pkgs; [
+        libxml2
+        icu
+      ];
     };
 
     gamemode = pkgs.lib.mkIf (config.dgbCustom.enableGaming && pkgs.stdenv.hostPlatform.isx86_64) {
