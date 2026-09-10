113eb7e60774dba874a8e9691d181c7e8a3fb003
Dillon Beliveau <dillon@dgb.sh>
2026-09-10 14:54:24 -0700

    Update nvidia driver

--- nix/hosts/battlestation.nix
+++ nix/hosts/battlestation.nix
@@ -7,7 +7,15 @@
   ...
 }:
 let
-  nvidia-driver = config.boot.kernelPackages.nvidiaPackages.latest;
+  # nvidia-driver = config.boot.kernelPackages.nvidiaPackages.latest;
+  nvidia-driver = config.boot.kernelPackages.nvidiaPackages.mkDriver {
+    version = "615.71.09";
+    sha256_64bit = "sha256-zc7tIrvrYSSNGm3qvCWWZz46ZQFpjucayNL9wo87cP4=";
+    sha256_aarch64 = "sha256-IbekQhE7cFfmnPZaLY9NDYcF7CoNZ+2Qb7sRd4EOgWM=";
+    openSha256 = "sha256-3gByMYIwFzRaLdDG+roCEOuKRRJDrljG9AlLnRZTirM=";
+    settingsSha256 = "sha256-LK1LU8mDkM/XVRKPBtuOZh9nIP/lGFLAJnmasEX8jhg=";
+    persistencedSha256 = "sha256-qPRb+3d88+2RcpUkoBTbjIaImnQ+jX+/6p1vXcJ5geE=";
+  };
 in
 
 {
